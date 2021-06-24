c9df41ca1227ddec3da9cfad25d90d18095fc9e3
OmmziDeveloper <58804516+OmmziDeveloper@users.noreply.github.com>
2021-06-24 16:24:03 +0530

    basic trasitions and code completed

---
+++ app.js
@@ -0,0 +1,76 @@
+// Selecting elemenets from html(index)
+const todoInput = document.querySelector('.todo-input');
+const todoButton = document.querySelector('.todo-button');
+const todoList = document.querySelector('.todo-list');
+
+console.log(todoButton);
+
+//Adding Event Listeners to the elements grabbed
+
+todoButton.addEventListener('click',addTodo);
+todoList,addEventListener('click',deleteCheck);
+
+
+
+
+//functions 
+function addTodo(e)
+{ e.preventDefault();
+    if(todoInput.value=="")
+    {
+        alert('please write a task! :)');
+    }
+    else{
+    console.log('voo');
+
+    //create todo DIV
+    const todoDiv = document.createElement('div');
+    todoDiv.classList.add('todo');
+
+    // create LI(child of div)
+    const newTodo = document.createElement('li');
+    newTodo.innerText= todoInput.value;
+    newTodo.classList.add('todo-item');
+    todoDiv.appendChild(newTodo); //making the li a child of div
+
+    //complete button check 
+    const completedButton = document.createElement('button');
+    completedButton.innerHTML = `<i class = 'fas - fa-check'></i>`
+    completedButton.classList.add('completed-btn');
+    todoDiv.appendChild(completedButton);
+
+    //trash button
+
+    const trashButton = document.createElement('button');
+    trashButton.innerHTML = `<i class = 'fas - fa-trash'></i>`
+    trashButton.classList.add('trash-btn');
+    todoDiv.appendChild(trashButton);
+
+//now the div we have created is actually just inside the body, we need to append it to the ul
+
+todoList.appendChild(todoDiv);
+todoInput.value="";
+
+}
+
+}
+
+    function deleteCheck(e)
+    {
+        const item = e.target;
+        console.log(e.target);
+        //delete item 
+        if(item.classList[0]==='trash-btn')
+        {
+            const todo = item.parentElement;//get parent element 
+            todo.remove();//removes item 
+        
+        }
+
+        //checked item
+        if(item.classList[0]==='completed-btn')
+        {
+            const todo = item.parentElement;
+            todo.classList.toggle('completed');
+        }
+    }
